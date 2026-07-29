1c52e42b540928f284efaebc9b80f6b764f60e31
ester.miranda99 <ester.miranda.cobos@gmail.com>
2026-07-29 16:26:17 -0300

    Exercício básico para reforçar aprendizado da ESTRUTURA REPETITIVA FOR

---
+++ 01-fundamentos/ContagemRegressiva.java
@@ -0,0 +1,16 @@
+// Programa para praticar a ESTRUTURA REPETITIVA FOR. 
+// Contagem regressiva de um foguete fictício.
+
+public class ContagemRegressiva {
+	public static void main(String [] args) {
+		System.out.println("----- PREPARAR PARA O LANÇAMENTO... ----- ");
+		
+		// Início      | Condição   | Passo
+		for (int i = 10; i >= 0; i--) {
+			System.out.println(i);
+		}
+		
+		System.out.println("🚀 DECOLAR!");
+		
+	}
+}
